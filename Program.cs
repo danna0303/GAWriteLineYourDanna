f3ad04770e1498f8d61504cee1c69f42d463f6f2
dpech <dpech@DANNA>
2024-01-15 23:44:19 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GAWriteLineYourDanna
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Edsger W. Dijkstra:");
+            Console.WriteLine("Computer science is no more about computers than astronomy is about telescopes");
+            Console.WriteLine("");
+            Console.Write("Alan Kay:");
+            Console.WriteLine("The best way to predict the future is to invent it.");
+            Console.WriteLine("");
+            Console.Write("Grace Hopper:");
+            Console.WriteLine("The most damaging phrase in the language is, We’ve always done it this way.");
+            Console.WriteLine("");
+            Console.Write("Terry Pratchett (Discworld):");
+            Console.WriteLine("It's not worth doing something unless someone, somewhere, would much rather you weren't doing it.");
+
+        }
+    }
+}
